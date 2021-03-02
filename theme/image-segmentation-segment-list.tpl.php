9b3c4367d42130713563c09d0807f50192e75f10
Space Duck Devourer of Worlds <alexandergcairns@gmail.com>
2021-03-02 00:05:40 -0400

    Stub out image segment list block

---
+++ theme/image-segmentation-segment-list.tpl.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ * @file
+ * This is the template file for the corpus sidebar .
+ */
+?>
+
+<div id="sidebar_container">
+
+    <!--Filter Sidebar Section-->
+    <div class="segment_list_section">
+        <h2>Filter</h2>
+        <p>Filter by category:</p>
+        <select name="extract_image_category" id="extract_image_category">
+          <?php foreach ($content_types as &$type) : ?>
+              <option value=<?php $type ?>><?php print $type ?></option>
+          <?php endforeach ?>
+        </select>
+        <br/>
+        <br/>
+        <button id="sidebar_filter_apply_button">Apply</button>
+    </div>
+
+    <!--Segments Sidebar Section-->
+    <div class="segment_list_section">
+        <h2>Segments</h2>
+        <div id="segment_list_gallery">
+          <?php foreach ($segments as &$segment) : ?>
+              <div class="segment_preview">
+                  <h3><?php print $segment['type'] ?>:</h3>
+                  <a href=<?php $segment['link'] ?>>
+                    <?php print theme('image', [
+                      'path' => $segment['tn_url'],
+                      'title' => $segment['object']->label,
+
+                    ]) ?>
+                  </a>
+              </div>
+          <?php endforeach ?>
+        </div>
+    </div>
+</div>
